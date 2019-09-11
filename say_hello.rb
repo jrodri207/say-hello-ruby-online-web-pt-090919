4b48e51b52b68afcb8cf440aada8b6a81cbf904e
Joshua Rodriguez <josh030596@gmail.com>
2019-09-11 04:42:56 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,10 +1,8 @@
 def say_hello(name) 
   puts "Hello #{name}!"
-  say_hello("Kent Beck")
 end
 
 def hello_with_option(name = "Ruby Programmer")
   puts "Hello #{name}!"
-  hello_with_option() 
 end 
   
